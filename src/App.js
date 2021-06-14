c7817c6650d3d277c9c3ffd63eaef3c94ae561d0
abdu-zeyad <abd.d@live.com>
2021-06-14 14:23:22 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import Card from 'react-bootstrap/Card';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -21,13 +26,17 @@ class App extends React.Component {
       console.log(locResult.data);
       this.setState({
         locData: locResult.data[0],
-        displayMap: true
+        displayMap: true,
+        displayErrMsg: false
+
       })
     }
     catch {
       this.setState({
         errMsg: 'error this is a bad response',
-        displayErrMsg: true
+        displayErrMsg: true,
+        displayMap: false
+
       })
     }
 
@@ -36,18 +45,35 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.getLocation}>
-          <input type='text' placeholder='city name' name='searchQuery' />
-          <input type='submit' value='search' />
-        </form>
+        <Form onSubmit={this.getLocation}>
+          <InputGroup className="mb-3">
+            <Button type='submit' value='search' variant="outline-secondary" id="button-addon1">
+              Search
+            </Button>
+            <FormControl
+              aria-label="Example text with button addon"
+              aria-describedby="basic-addon1"
+              type='text' placeholder='city name' name='searchQuery'
+            />
+          </InputGroup>
+        </Form>
+
+        <Card style={{ width: '18rem' }}>
+          {this.state.displayMap && <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.43fed3791d35ddb76aa14f749c6d3080&center=${this.state.locData.lat},${this.state.locData.lon}`} alt='map' />}
+          <Card.Body>
+            <Card.Title> {this.state.displayMap && this.state.locData.display_name}</Card.Title>
+            <Card.Text>
+
+              {this.state.displayMap && this.state.locData.lon}
+              {this.state.displayMap && this.state.locData.lat}
+              {this.state.displayErrMsg && this.state.errMsg}
+
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
 
 
-        <p>{this.state.locData.display_name}</p>
-        <p>{this.state.locData.lon}</p>
-        <p>{this.state.locData.lat}</p>
-        {this.state.displayMap && <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.43fed3791d35ddb76aa14f749c6d3080&center=${this.state.locData.lat},${this.state.locData.lon}`} alt='map' />}
-        {this.state.displayErrMsg && this.state.errMsg}
       </div>
     )
   }
@@ -56,16 +82,3 @@ class App extends React.Component {
 export default App;
 
 
-
-// {/* <Form onSubmit={this.getLocation}>
-//   <Form.Group className="mb-3" controlId="formBasicEmail">
-//     <Form.Label>write the city's name</Form.Label>
-//     <Form.Control type="email" placeholder="Enter email" />
-//     <Form.Text className="text-muted">
-//       Search for a city
-//     </Form.Text>
-//   </Form.Group>
-//   <Button variant="primary" type="submit" onClick={this.getLocation}>
-//     Search
-//   </Button>
-// </Form> */}
